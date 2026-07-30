da6e07827dbff46dd86965795a218f0ba93d89cd
root <root@guoj.guoj>
2026-07-29 23:13:02 -0400

    add check_time relate

--- code/src/common.h
+++ code/src/common.h
@@ -17,16 +17,30 @@ inline double pct(double target, double base){
 }
 
 
-inline bool is_am_time(const tickTime& t) {
-    if (t.hour < 12){
-        return true;
-    }else{
-        return false;
+inline int check_time(const tickTime& self, const tickTime& that) {
+    if (self.hour != that.hour) {
+        return self.hour - that.hour; // 小时不同直接比小时
     }
+    return self.minute - that.minute; // 小时相同时比分钟
+}
+
+inline bool is_am_time(const tickTime& t) {
+
+    tickTime that_time{11, 30};
+
+    if (check_time(t, that_time) <= 0){
+        return true;
+    }
+
+    return false;
+
 }
 
 inline bool after_15(const tickTime& t) {
-    if (t.hour == 15 && t.minute !=0){
+
+    tickTime that_time{15, 00};
+
+    if (check_time(t, that_time) > 0){
         return true;
     }else{
         return false;
@@ -34,8 +48,10 @@ inline bool after_15(const tickTime& t) {
 }
 
 inline bool is_am_end(const tickTime& now,const tickTime& pre) {
-    if (now.hour >= 13 && pre.hour <= 11 ){
-        //  std::cout << "record_change for is am end :" << now.hour <<":"<< now.minute << std::endl;
+
+    tickTime that_time{11, 30};
+
+    if (check_time(now, that_time) > 0 &&  check_time(pre, that_time) <= 0){
         return true;
     }else{
         return false;
